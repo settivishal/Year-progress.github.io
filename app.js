4be8155c4dcf8dd06bd8ecf2508b1539cd5c8fad
vishalsetti <vishalsetti2000@gmail.com>
2021-06-10 22:40:23 +0530

    dynamic year selection

--- app.js
+++ app.js
@@ -2,27 +2,15 @@ const meter = document.querySelector(".meter-bar");
 const value = document.querySelector('.value');
 
 // ---------get dates
-let thisYear = new Date("Jan 01,2021 00:00:00");
-console.log(thisYear);
+let thisYear = new Date(new Date().getFullYear(), 0, 1).getTime();//* gets first day of the year;
+let nextYear = new Date(new Date().getFullYear() + 1, 0, 1).getTime();//* gets first day next of the year;
 
 let current = new Date().getTime();
 
-let remaining = current - thisYear;
-
-let days = Math.floor(remaining/(1000*60*60*24));
-
-// ----------check for leap year
-let noOfDays;
-let currentYear = new Date().getFullYear();
-if(((currentYear%4==0)&&(currentYear%100!=0))||(currentYear%400==0)){
-    noOfDays = 366;
-}
-else{
-    noOfDays = 365;
-}
+// let days = Math.floor(remaining/(1000*60*60*24));
 
 // ---------calculation percentage
-let fractionDays = ((days+1)/noOfDays).toFixed(3);
+let fractionDays = ((current-thisYear)/(nextYear-thisYear)).toFixed(3);
 
 meter.style.width = (fractionDays*100).toFixed(2) + "%";
 value.innerHTML = (fractionDays*100).toFixed(2) + "% Finished";
